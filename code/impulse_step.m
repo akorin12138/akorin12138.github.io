7ee9f4265870a315e105161af6effd6681efe0dd
akorin12138 <niozi@outlook.com>
2025-03-07 19:51:08 +0800

    update

--- code/impulse_step.m
+++ code/impulse_step.m
@@ -13,8 +13,13 @@ b=1;        % 系统函数的分子
 
 sys=tf(b,a);
 
+T=1e-4;
+t=0:T*2.5e-3:2.5e-3;
+x=square(2*pi/T*t,50); % 方波信号作为输入信号
+
 [h0,t0]=impulse(sys);
 [h1,t1]=step(sys);
+[h2,t2]=lsim(sys,x,t);
 
 %画图
 figure;
@@ -23,3 +28,10 @@ title('单位冲激响应');
 figure;
 plot(t1, h1);
 title('单位阶跃响应');
+figure;
+subplot(2, 1, 1);
+plot(t2, h2);
+title('全响应');
+subplot(2, 1, 2);
+plot(t, x);
+title('输入信号');
