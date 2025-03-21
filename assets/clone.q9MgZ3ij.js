28fe1953fee39aa2a6673dacb40d25a8b6e3e4ea
akorin12138 <akorin12138@users.noreply.github.com>
2025-03-21 15:00:33 +0000

    deploy: 53cde3bc8e076985cdd3ff9ff79d475d99a31bee

---
+++ assets/clone.q9MgZ3ij.js
@@ -0,0 +1,1 @@
+import{b as r}from"./_baseUniq.cMR-sMMA.js";var e=4;function a(o){return r(o,e)}export{a as c};
---
+++ assets/post.DDFIMpWk.js
@@ -0,0 +1,1 @@
+import"./chunks/@vueuse/motion.Dq56t8Eg.js";import{a as t}from"./app.C809K6Na.js";import{A as e}from"./framework.CLp-3oYl.js";function l(o){if(!o)return{color:"",icon:"",styles:void 0};const r=t();o in r.value.types||(o="link");const s=r.value.types[o].color,i=r.value.types[o].icon,n=e(()=>o?{"--card-c-primary":s}:void 0);return{color:s,icon:i,styles:n}}export{l as u};
